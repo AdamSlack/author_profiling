e50ca704f4d58b8731d4f1b4ee61644026955e63
Adam Slack <adamslack@outlook.com>
2017-12-21 13:46:06 +0000

    emotive scores and overall sentiment calculated

--- src/data_stats.py
+++ src/data_stats.py
@@ -5,6 +5,28 @@ from functools import reduce
 import string
 from json import dumps
 from itertools import product
+from collections import defaultdict
+
+def parse_emo_lex():
+    """ parses emotive lexicon into dict of words with a dict of their emotive tags """
+    emolex = defaultdict(lambda : defaultdict(int))
+    with open('../data/emolex.tsv') as f:
+        for line in f:
+            components = line.strip().split('\t')
+            try:
+                emolex[components[0]][components[1]] = int(components[2])
+            except:
+                print(line)
+                print(components)
+                raise
+    return emolex
+
+def emo_lex_emotions(emo_lex = None):
+    if not emo_lex:
+        emo_lex = parse_emo_lex()
+
+    return list(emo_lex[list(emo_lex.keys())[0]].keys())
+    
 
 def upenn_tags():
     tag_dict = load('help/tagsets/upenn_tagset.pickle')
@@ -96,31 +118,70 @@ def count_trigrams(tokenized_review : 'list of list of str'):
             trigram_counts[format_trigram_key(x,y,z)] += 1   
     return trigram_counts
 
+def emotive_scores(tokenized_review : 'list of list of str', emo_lex=None):
+    if not emo_lex:
+        emo_lex = parse_emo_lex()
+
+    review_emotive_counts = {emote : 0 for emote in emo_lex_emotions(emo_lex)}
+
+    for sent in tokenized_review:
+        for word in sent:
+            if word.lower() in emo_lex:
+                for key, val in emo_lex[word.lower()].items():
+                    review_emotive_counts[key] += val
+
+    return review_emotive_counts
+
+def sentiment_score(tokenized_review : 'list of list of str', emo_lex=None, emotive_scores=None ):
+    if not emo_lex and not emotive_scores:
+        emo_lex = parse_emo_lex()
+
+    if not emotive_scores:
+        emotive_scores = emotive_scores(tokenized_review, emo_lex)
+
+    pos = emotive_scores['positive']
+    neg = emotive_scores['negative']
+
+    return (pos - neg) / (pos + neg)
+
 def main():
     """ main program flow. """
-    review = 'This is a sentence in a review. This is another sentence in the same review.'
+    review = 'The book was brilliant, i really enjoyed reading it. It was delightful to be presented with characters as developed as those in this story. The author was masterful in their ability to craft a world so full of wonders. The only critisism that i have is that it was not long enough, i am distressed to think that the story is already over.'
 
+    # review init
     tokenized_review = tokenize(review)
-    word_count = count_words(tokenized_review)
-    sent_count = count_sentences(tokenized_review)
-    avg_sent_len = average_sentence_length(tokenized_review)
-    char_count = count_word_characters(tokenized_review)
-    avg_word_len = average_word_length(tokenized_review)
-    unigram_counts = count_unigrams(tokenized_review)
-    bigram_counts = count_bigrams(tokenized_review)
-    trigram_counts = count_trigrams(tokenized_review)
-
     print(review)
     print('Tokens: ')
     print(tokenized_review)
-    print('Word Count:', str(word_count))
-    print('Sent Count:', str(sent_count))
-    print('Average Sentence Length:', str(avg_sent_len))
-    print('Character Count:', str(char_count))
-    print('Average Word Length:', str(avg_word_len))
-    print(dumps(unigram_counts,indent=2))
-    print(dumps(bigram_counts,indent=2))
-    print(dumps(trigram_counts, indent=2))
+
+    # Surface Stats
+    #word_count = count_words(tokenized_review)
+    #sent_count = count_sentences(tokenized_review)
+    #avg_sent_len = average_sentence_length(tokenized_review)
+    #char_count = count_word_characters(tokenized_review)
+    #avg_word_len = average_word_length(tokenized_review)
+    #print('Word Count:', str(word_count))
+    #print('Sent Count:', str(sent_count))
+    #print('Average Sentence Length:', str(avg_sent_len))
+    #print('Character Count:', str(char_count))
+    #print('Average Word Length:', str(avg_word_len))
+
+    # POS Tag Stats
+    #unigram_counts = count_unigrams(tokenized_review)
+    #bigram_counts = count_bigrams(tokenized_review)
+    #trigram_counts = count_trigrams(tokenized_review)
+    #print(dumps(unigram_counts,indent=2))
+    #print(dumps(bigram_counts,indent=2))
+    #print(dumps(trigram_counts, indent=2))
+
+    # Emotive Stats
+    emo_lex = parse_emo_lex()
+    review_emotive_counts = emotive_scores(tokenized_review, emo_lex)
+    review_sentiment_score = sentiment_score(tokenized_review, emotive_scores=review_emotive_counts)
+    print(emo_lex_emotions(emo_lex))
+    print(dumps(review_emotive_counts,indent=2))
+    print('Sentiment Score: ' + str(review_sentiment_score))
+
 
 if __name__ == '__main__':
     main()
